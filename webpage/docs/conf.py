f976578ee57cf50b10632b3d4ef7e49f9e9126f1
wellbuoy <152445985+wellbuoy@users.noreply.github.com>
2024-01-24 03:58:19 -0800

    Update conf.py

--- webpage/docs/conf.py
+++ webpage/docs/conf.py
@@ -10,7 +10,7 @@ import os
 import sys
 import django
 sys.path.insert(0, os.path.abspath('..'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'Your_project_name.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'webpage.settings'
 django.setup()
 
 project = 'webpage'
